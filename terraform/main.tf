1bab335b83db54eb4c727cfc50dda2415681f5d4
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2025-02-14 12:58:21 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = var.region 
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" 
+  ami           = var.ami 
   instance_type = "t2.micro" 
 
   tags = {
