c7b3a329b84ffb6f4d4c557dced9683834a31277
Amanda Carmo <amandaleandrosoares@gmail.com>
2025-02-14 17:07:03 -0300

    ajuste

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,34 @@
 provider "aws" {
-  region = var.region 
+  region = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = var.ami 
-  instance_type = "t2.micro" 
-  subnet_id     = "subnet-0d712e31945668d8b"
+
+resource "aws_vpc" "vpcExampleSimple" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Ec2-Instance-ExampleSimple"
+    Name = var.vpc
+  }
+}
+
+# Criar a Subnet
+resource "aws_subnet" "subnetExampleSimple" {
+  vpc_id            = aws_vpc.var.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a" 
+
+  tags = {
+    Name = "example_subnet"
+  }
+}
+
+# Criar a Instância EC2
+resource "aws_instance" "example_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.example_subnet.id
+
+  tags = {
+    Name = var.instance_name
   }
 }
