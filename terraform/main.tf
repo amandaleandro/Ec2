5bcdc47ca6c414446839dee38f41d939de724337
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2025-02-14 14:52:25 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami 
   instance_type = "t2.micro" 
+  subnet_id = subnet-0d712e31945668d8b
 
   tags = {
     Name = "Ec2-Instance-ExampleSimple"
